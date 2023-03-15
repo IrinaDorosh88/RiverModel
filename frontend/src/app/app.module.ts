ac8f5297e44652b297a6c0761804b8be5afacac4
Pavliuk Maksym <pavliuk.maksym@chnu.edu.ua>
2023-03-15 21:53:39 +0200

    added angular material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ENVIRONMENT_INITIALIZER } from './app-init';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ENVIRONMENT_INITIALIZER } from './app-init';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [ENVIRONMENT_INITIALIZER],
   bootstrap: [AppComponent],
 })
