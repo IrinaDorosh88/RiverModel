814807a1067d97215e167d461dfbf5e4d193d517
Mykolai Buriak <nikolaybeet@gmail.com>
2023-05-03 22:47:11 +0000

    Add Role's CRUD

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers.user import router as users_router
 from routers.auth import router as auth_router
 from routers.river import router as river_router
+from routers.role import router as role_router
 
 
 app = FastAPI()
@@ -24,3 +25,4 @@ app.add_middleware(
 app.include_router(auth_router, prefix='/auth', tags=['auth'])
 app.include_router(users_router, prefix='/users', tags=['users'])
 app.include_router(river_router, prefix='/rivers', tags=['rivers'])
+app.include_router(role_router, prefix='/roles', tags=['roles'])
