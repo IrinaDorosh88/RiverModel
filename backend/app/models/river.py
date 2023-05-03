fd238021fae52c744df7350f017ae36946c17c63
Andriy Meleniuk <amelentium@gmail.com>
2023-05-03 21:41:57 +0300

    Refactoring after PR review

--- backend/app/models/river.py
+++ backend/app/models/river.py
@@ -1,14 +1,17 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, Sequence
 from sqlalchemy.sql import func
 from core.db import Base
 
 
-class River(Base):
-    __tablename__ = "river"
+table_name = 'rivers'
 
-    id = Column(Integer, primary_key=True, index=True)
+class River(Base):
+    __tablename__ = "rivers"
+
+    id = Column(Integer, Sequence(f'{table_name}_id_seq'), primary_key=True, nullable=False)
     name = Column(String(50), nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    modified_at = Column(DateTime(timezone=True), onupdate=func.now())
+
     is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    modified_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
   
--- backend/app/routers/river.py
+++ backend/app/routers/river.py
@@ -7,14 +7,18 @@ from services.river import RiverService
 
 router = APIRouter()
 
-@router.get("/", response_model=list[River])
+@router.get("/", description="This endpoint returns all river data.",
+            response_model=list[River])
 def get_rivers(service: RiverService = Depends()):
     return service.get_rivers()
 
-@router.post("/", response_model=River)
+@router.post("/", description="This endpoint creates a new river with the provided information and returns the river's data.",
+             response_model=River)
 def create_river(river_data: RiverCreate, service: RiverService = Depends()):
     return service.create_river(river_data=river_data)
 
-@router.patch("/{river_id}", response_model=River)
+@router.patch("/{river_id}", description="This endpoint updates river information with the given ID. "
+                                         "The request body should contain a JSON object with the fields to be updated.",
+               response_model=River)
 def update(river_id: int, river_data: RiverUpdate, service: RiverService = Depends()):
     return service.update_river(river_id=river_id, river_data=river_data)
