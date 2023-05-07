42b7fd3236c71753598dd81891bc0d33cb2533f3
heroicfun <tracaleks8@gmail.com>
2023-05-07 08:09:26 +0000

    Resolved problems with migrations

--- backend/app/models/chemical_element.py
+++ backend/app/models/chemical_element.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Numeric, DateTime, Boolean, Sequence
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, Boolean, Sequence, UniqueConstraint
 from sqlalchemy.sql import func
 from core.db import Base
 
