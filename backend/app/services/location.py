83854f92676364c28f2faef2527405c68ecf8f78
projectDNK <den.deineko@gmail.com>
2023-05-12 20:34:04 +0300

    Add association table LocationChemicalElements

--- backend/app/services/location.py
+++ backend/app/services/location.py
@@ -1,11 +1,12 @@
 from fastapi import HTTPException, status
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import joinedload
 
 from services import AppService
-from models.location import Location
-from models.river import River
+from models import Location, River, ChemicalElement, LocationChemicalElements
 from schemas import PaginationParams
 from schemas.location import LocationCreate, LocationUpdate, PaginatedLocation
-from sqlalchemy.exc import IntegrityError
+
 
 unique_violation_error = HTTPException(
     status_code=status.HTTP_400_BAD_REQUEST,
@@ -19,7 +20,7 @@ class LocationService(AppService):
         return self.session.query(Location).get(location_id)
 
     def get_locations(self, river_id: int, pagination: PaginationParams):
-        query = self.session.query(Location)
+        query = self.session.query(Location).options(joinedload(Location.chemical_elements))
 
         is_paginated_response = isinstance(pagination.limit, int) and isinstance(pagination.offset, int)
 
@@ -28,13 +29,15 @@ class LocationService(AppService):
 
         query = query.order_by(Location.name.asc())
 
+        total = query.count()
+
         if is_paginated_response:
             query = query.limit(pagination.limit).offset(pagination.offset)
 
         data = query.all()
 
         return PaginatedLocation(
-            total=query.count(),
+            total=total,
             limit=pagination.limit,
             offset=pagination.offset,
             data=data
@@ -44,9 +47,18 @@ class LocationService(AppService):
         if self.session.query(River).get(location_data.river_id) is None:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Specified an unknown river')
 
-        db_location = Location(**location_data.dict())
+        location_data_dict = location_data.dict()
+        input_chemical_elements = location_data_dict.pop('chemical_elements')
+        db_chemical_elements = self.session.query(ChemicalElement) \
+            .filter(ChemicalElement.id.in_(input_chemical_elements)).all()
+
+        if len(db_chemical_elements) != len(input_chemical_elements):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Specified an unknown chemical element')
 
         try:
+            db_location = Location(**location_data_dict)
+            for db_chemical_element in db_chemical_elements:
+                db_location.chemical_elements.append(db_chemical_element)
             self.session.add(db_location)
             self.session.commit()
             self.session.refresh(db_location)
@@ -59,9 +71,23 @@ class LocationService(AppService):
         if self.session.query(River).get(location_data.river_id) is None:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Specified an unknown river')
 
+        location_data_dict = location_data.dict(exclude_unset=True)
+        input_chemical_elements = location_data_dict.pop('chemical_elements')
+        db_chemical_elements = self.session.query(ChemicalElement) \
+            .filter(ChemicalElement.id.in_(input_chemical_elements)).all()
+
+        if len(db_chemical_elements) != len(input_chemical_elements):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Specified an unknown chemical element')
+
         db_location = self.session.query(Location).get(location_id)
-        for field, value in location_data.dict(exclude_unset=True).items():
+        for field, value in location_data_dict.items():
             setattr(db_location, field, value)
+
+        db_location.chemical_elements = [
+            db_chemical_element for db_chemical_element in db_chemical_elements
+            if db_chemical_element.id in input_chemical_elements
+        ]
+
         try:
             self.session.commit()
             self.session.refresh(db_location)
@@ -73,6 +99,7 @@ class LocationService(AppService):
     def delete_location(self, location_id: int):
         db_location = self.get_location_by_id(location_id)
         if db_location:
+            db_location.chemical_elements = []
             self.session.delete(db_location)
             self.session.commit()
         else:
