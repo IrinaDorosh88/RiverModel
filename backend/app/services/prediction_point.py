2bd7a94f4f01d890d9b6d48fb036fc3d45039b21
Mykolai Buriak <nikolaybeet@gmail.com>
2023-05-14 13:57:45 +0000

    remove validate prediction points method

--- backend/app/services/prediction_point.py
+++ backend/app/services/prediction_point.py
@@ -8,8 +8,6 @@ from rivermodel import RiverModel
 
 class PredictionPointService(AppService):
     def create_prediction_points(self, prediction_point_data: PredictionPointCreate) -> PredictionPointModel:
-#        self.validate_dependencies(prediction_point_data=prediction_point_data)
-
         prediction_points = [
             PredictionPointModel(**prediction_point.dict(), measurement_id=prediction_point_data.measurement_id)
             for prediction_point in prediction_point_data.values
@@ -21,11 +19,6 @@ class PredictionPointService(AppService):
             self.session.refresh(prediction_point)
 
         return prediction_points
-
-    def validate_dependencies(self, prediction_point_data: PredictionPointCreate):
-        db_measurement = self.session.query(Measurement).get(prediction_point_data.measurement_id)
-        if not db_measurement:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Specified an unknown measurement')
 
     def run_model(self, data, critical_value):
         time = 0
