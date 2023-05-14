9499fae121e7b2ab731509056dd6dd509c65d827
heroicfun <101557656+heroicfun@users.noreply.github.com>
2023-05-14 12:08:01 +0300

    Add rivermodel package source files (#28)

---
+++ package/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='rivermodel',
+      version='0.1',
+      description='Package for solving water grade quality problem',
+      author='heroicfun',
+      author_email='baier.oleksandr@chnu.edu.ua',
+      packages=find_packages())
